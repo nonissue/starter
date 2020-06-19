e5b59e470772420a446fd25389364bc587434433
nonissue <andy@nonissue.org>
2020-06-18 22:10:12 -0600

    Cleanup unused code

--- src/components/OptionsList.js
+++ src/components/OptionsList.js
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 // import { SearchMajorMonotone } from "@shopify/polaris-icons";
-import styles from "./OptionsList.module.css";
+import styles from './OptionsList.module.css';
 // const testData = require("../data.json");
 
 /*
@@ -23,68 +23,33 @@ Gets rendered twice?
  * Err, how do we actually handle the change?
  *
  * @param {Array} data array of objects of form { label, value } NOTE: First object has to match initial state
- *
+ * @param {Number} selected index of selected option
  * @return {ReactComponent} returns react component
  */
 
 // eslint-disable-next-line no-unused-vars
 const OptionsList = ({ data, selected }) => {
-  // const [query, setQuery] = useState();
-  // const [results, setResults] = useState([]);
-
-  // const handleChange = e => {
-  //   setUserInput({ value: e.currentTarget.value });
-
-  //   const searchString = e.currentTarget.value.toLowerCase();
-
-  //   let tempResults = testData.filter(d => {
-  //     if (
-  //       (d.name.toLowerCase().includes(searchString) ||
-  //         d.url.includes(searchString)) &&
-  //       e.currentTarget.value.length > 1
-  //     ) {
-  //       return d;
-  //     }
-  //     return undefined;
-  //   });
-
-  //   console.log(tempResults);
-
-  //   setResults(tempResults);
-  // };
-
-  // const clearSearch = e => {
-  //   console.log("clearSearch fired");
-  //   setUserInput({ value: "" });
-  //   setResults([]);
-  // };
-
   return (
     <>
-      {/* Break this out to a new component, optionslist? */}
-      {/* {console.log("Selected: " + selected)} */}
       {data.length !== 0 && (
-        <div className={`${styles["results-list"]}`}>
+        <div className={`${styles['results-list']}`}>
           <ul>
             {data.map((d, i) => {
               return (
                 <li key={d.name}>
                   <div
-                    className={`${styles["results-item"]} ${
+                    className={`${styles['results-item']} ${
                       i + 1 === selected && styles.selected
                     }`}
                   >
-                    <div className={styles["item-name"]}>
-                      {d["name"]}
+                    <div className={styles['item-name']}>
+                      {d['name']}
                       &nbsp;&nbsp;
-                      <div className={styles["shortcut-label"]}>
+                      <div className={styles['shortcut-label']}>
                         &#8984; + {i + 1}
                       </div>
                     </div>
-                    {/* <div className={styles["item-category"]}>
-                      {d["category"]}
-                    </div> */}
-                    <div className={styles["item-url"]}>{d["url"]}</div>
+                    <div className={styles['item-url']}>{d['url']}</div>
                   </div>
                 </li>
               );
@@ -97,11 +62,12 @@ const OptionsList = ({ data, selected }) => {
 };
 
 OptionsList.defaultProps = {
-  data: "Enter text",
+  data: [],
 };
 
 OptionsList.propTypes = {
   data: PropTypes.array,
+  selected: PropTypes.number,
 };
 
 export default OptionsList;
